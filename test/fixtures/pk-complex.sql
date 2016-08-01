bee066fbe119e83af07458931aadee5c1fa5309e
Emilio <Platzer>
2016-08-01 15:08:09 -0300

    fixtures para pk #1

---
+++ test/fixtures/pk-complex.sql
@@ -0,0 +1,14 @@
+create table "example-one" (
+ "text-field" text,
+ "int-field" integer,
+ "num" numeric,
+ "big" bigint,
+ "double" double precision,
+ primary key ("text-field", "int-field", "num")
+);
+
+insert into "example-one" ("text-field", "int-field", "num", "big", "double") values
+ ('hello', 1, 3.141592, 1234567890, 1.12e-101),
+ ('ciao', 2, 3, 0, 0.0),
+ ('hello', 1, 2, null, null),
+ ('ciao', 2, 4, 0, 0.0);
---
+++ test/fixtures/pk-simple.sql
@@ -0,0 +1,12 @@
+create table "example-one" (
+ "text-field" text,
+ "int-field" integer,
+ "num-field" numeric,
+ "big" bigint,
+ "double" double precision,
+ primary key ("text-field")
+);
+
+insert into "example-one" ("text-field", "int-field", "num-field", "big", "double") values
+ ('hello', 1, 3.141592, 1234567890, 1.12e-101),
+ ('ciao', null, null, 0, 0.0);
