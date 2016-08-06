8c5353c80cbe6868968c283e7624e428f5639f4d
Emilio <emilioplatzer@gmail.com>
2016-08-05 22:40:57 -0300

    corrijo la identación de tools/web.js

--- tools/web.js
+++ tools/web.js
@@ -15,6 +15,7 @@ function doStylus(str) {
         });
     });
 }
+
 function generateWeb() {
     console.log("Generating web content...");
     var srcDir=process.cwd()+'/src';
@@ -29,35 +30,35 @@ function generateWeb() {
             }).then(function(content) {
                 var ext = Path.extname(file).substring(1);
                 return { fullName:fullName,
-                         baseName:Path.basename(file, ext),
-                         ext:ext,
-                         content:content
-                       };
+                    baseName:Path.basename(file, ext),
+                    ext:ext,
+                    content:content
+                };
             }).catch(function(err) {
                 console.log("error", err)
             });
-       })).then(function(files) {
-           return Promises.all(files.map(function (file) {
-               return Promises.start(function() {
-                   switch(file.ext) {
-                       // jade parsing
-                       case 'jade': file.ext = 'html'; return { data:pug.render(file.content, {}) };
-                       // stylus parsing
-                       case 'styl': file.ext = 'css'; return doStylus(file.content).then(function(content){ return {data:content}}); 
-                       // estos son ignorados
-                       case 'pdn': return { skip:true };
-                       // estos son copiados tal cual
-                       default: return {};
-                   }
-               }).then(function(res) {
-                   if(! res.skip) {
-                       var destFile = Path.join(destDir, file.baseName+file.ext);
-                       if(res.data) { return fs.writeFile(destFile, res.data); }
-                       return fs.copy(file.fullName, destFile);
-                   }
-               });
-           }));
-       });
+        })).then(function(files) {
+            return Promises.all(files.map(function (file) {
+                return Promises.start(function() {
+                    switch(file.ext) {
+                        // jade parsing
+                        case 'jade': file.ext = 'html'; return { data:pug.render(file.content, {}) };
+                        // stylus parsing
+                        case 'styl': file.ext = 'css'; return doStylus(file.content).then(function(content){ return {data:content}}); 
+                        // estos son ignorados
+                        case 'pdn': return { skip:true };
+                        // estos son copiados tal cual
+                        default: return {};
+                    }
+                }).then(function(res) {
+                    if(! res.skip) {
+                        var destFile = Path.join(destDir, file.baseName+file.ext);
+                        if(res.data) { return fs.writeFile(destFile, res.data); }
+                        return fs.copy(file.fullName, destFile);
+                    }
+                });
+            }));
+        });
     });
 }
 
