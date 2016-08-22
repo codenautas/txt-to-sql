b5afa5bebb14525f0dc069e7478d2ac73a64975c
Diego F.(EW7) <diegoefe@gmail.com>
2016-08-22 16:20:26 -0300

    Más tests para letras acentuadas

--- test/fixtures/fields-lcalpha.sql
+++ test/fixtures/fields-lcalpha.sql
@@ -1,11 +1,13 @@
 create table "fields_lcalpha" (
  "cone_e" text,
  "esdrujulo" integer,
- "nada_raro" numeric,
- "mixto" bigint,
- "_1numeroyacento" double precision
+ "masdias" numeric,
+ "faltanaeeiiuoo" bigint,
+ "_1numeroyacento" double precision,
+ "maas" integer
 );
 
-insert into "fields_lcalpha" ("cone_e", "esdrujulo", "nada_raro", "mixto", "_1numeroyacento") values
- ('hello', 1, 3.141592, 1234567890, 1.12e-101),
- (null, null, null, 0, 0.0);
+insert into "fields_lcalpha" ("cone_e", "esdrujulo", "masdias", "faltanaeeiiuoo", "_1numeroyacento", "maas") values
+ ('hello', 1, 3.141592, 1234567890, 1.12e-101, null),
+ ('no toca botón', 2, 4.141592, 2234567890, 2.12e-101, null),
+ (null, null, null, 0, 0.0, null);
