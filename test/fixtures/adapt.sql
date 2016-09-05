59b1bcda7257cd82eee44ce66f6f73c04b1a068b
Diego F.(EW7) <diegoefe@gmail.com>
2016-09-05 09:53:52 -0300

    NULL's para adapt

--- test/fixtures/adapt.sql
+++ test/fixtures/adapt.sql
@@ -7,4 +7,7 @@ insert into "adapt" ("texts", "integers") values
  ('dos palabras', 1),
  ('una comilla''simple', 2),
  ('dos comillas"dobles"', 3),
- ('varios `, \ y /', 4);
+ ('varios `, \ y /', 4),
+ (null, 5),
+ ('% pct', null),
+ (null, null);
