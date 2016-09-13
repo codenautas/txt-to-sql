9ea812b965edbd943cea48046a52ef4785ee1063
Diego F.(EW7) <diegoefe@gmail.com>
2016-09-13 16:29:36 -0300

    Más pruebas con utf8-invalid

--- test/fixtures/utf8-invalid.sql
+++ test/fixtures/utf8-invalid.sql
@@ -1,7 +1,7 @@
 create table "utf8-invalid" (
  "int-field" integer(1),
- "text-field" character varying(18)
+ "text-field" character varying(16)
 );
 
 insert into "utf8-invalid" ("int-field", "text-field") values
- (1, 'puÃ±o y letra "â(¡"');
+ (1, '¡éste es el año!');
