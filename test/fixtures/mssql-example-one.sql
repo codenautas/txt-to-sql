26e55732778d2e993aad7131563aec57aabafc1e
Diego F.(EW7) <diegoefe@gmail.com>
2016-09-19 13:06:52 -0300

    Agrego generación de INSERT INTO tradicional para MSSQL

--- test/fixtures/mssql-example-one.sql
+++ test/fixtures/mssql-example-one.sql
@@ -6,6 +6,5 @@ create table [mssql-example-one] (
  [double col] real
 );
 
-insert into [mssql-example-one] ([text col], [int col], [num col], [big col], [double col]) values
- ('hello', 1, 3.141592, 1234567890, 1.12e-101),
- (null, null, null, 0, 0.0);
+insert into [mssql-example-one] ([text col], [int col], [num col], [big col], [double col]) values ('hello', 1, 3.141592, 1234567890, 1.12e-101);
+insert into [mssql-example-one] ([text col], [int col], [num col], [big col], [double col]) values (null, null, null, 0, 0.0);
