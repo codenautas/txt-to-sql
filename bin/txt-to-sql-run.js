054d5231a0db148486385c1552de0f44c9c41213
Diego F.(EW7) <diegoefe@gmail.com>
2016-10-12 07:25:50 -0300

    Preparando para #20

--- bin/txt-to-sql-run.js
+++ bin/txt-to-sql-run.js
@@ -6,10 +6,12 @@ var program = require('commander');
 var txtToSql = require('../lib/txt-to-sql.js');
 var Promises = require('best-promise');
 var fs = require('fs-promise');
+var fsSync = require('fs');
 var Path = require('path');
 var miniTools = require('mini-tools');
 var jsYaml = require('js-yaml');
-var changing = require('best-globals').changing;   
+var changing = require('best-globals').changing;
+var readline = require('readline');
 
 function getOutputDir(inFile) {
     return Promises.start(function() {
@@ -102,9 +104,61 @@ function doGenerate(params, inputYaml, create, inputName) {
     });
 }
 
-function doFast(params, inputName) {
+function fastProcessEncodingOptions(info) {
+    return txtToSql.getEncoding(info.rawTable).then(function(encoding) {
+        info.inputEncodingDetected = encoding;
+        if(! info.opts.inputEncoding) { info.opts.inputEncoding = info.inputEncodingDetected; }
+        if(! info.opts.outputEncoding) { info.opts.outputEncoding = info.inputEncodingDetected; }
+        //var inFromToString = info.rawTable.toString("utf8");
+        if(info.opts.inputEncoding==='ANSI') {
+            // if(inFromToString.substr(1).indexOf('\uFFFD')<0) {
+                // throw new Error('ansi -> utf8: replacement character not found');
+            // }
+            //info.decodedBuffer = iconv.decode(info.rawTable, "win1252");
+            // if(txtToSql.compareBuffers(info.decodedBuffer, info.rawTable) === -1) {
+                // throw new Error('ansi -> utf8: no conversion performed');
+            // }
+        } else if(info.opts.inputEncoding==='UTF8') {
+            //info.decodedBuffer = inFromToString;
+            // var result = txtToSql.compareBuffers(info.rawTable, new Buffer(info.decodedBuffer, 'utf8'));
+            // if(result !== -1) {
+                // throw new Error('utf8 check failed in position: '+result);
+            // }
+        } else {
+            //info.decodedBuffer = inFromToString;
+        }
+        return info;
+    });
+}
+
+function doFast(params, inputBase) {
+    var inStream, outStream;
+    var rl;
     return Promise.resolve().then(function() {
-        return "not yet";
+        return txtToSql.verifyInputParams(params);
+    }).then(fastProcessEncodingOptions)
+      .then(function(info) {
+        //console.log("info", info);
+        inStream = fsSync.createReadStream(inputBase+'.txt', {encoding:'utf8'});
+        outStream = fsSync.createWriteStream(inputBase+'.sql', {encoding:'utf8'});
+        rl = readline.createInterface({
+            input: inStream,
+            terminal: false
+        });
+        rl.on('line', function(line) {
+            console.log("line", line);
+            if(! info.headers) {
+                info.headers = line;
+                txtToSql.determineSeparator(info);
+                txtToSql.separateColumns(info);
+            } else {
+                
+            }
+            //outStream.write(line+'\n')
+        });
+        rl.on('close', function() {
+          console.log("info", info);  
+        });
     });
 }
 
@@ -130,7 +184,7 @@ getOutputDir(cmdParams.input).then(function(dir) {
     }).then(function(rawInput) {
         params.rawTable = rawInput;
         if(cmdParams.fast) {
-            return doFast(params, inputName);
+            return doFast(params, inputBase);
         } else if (cmdParams.prepare) {
             return doPrepare(params, inputYaml, createInputYaml);
         } else {
