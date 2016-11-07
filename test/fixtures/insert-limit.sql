2998db53d0e9006b97a15029597dbb12f54c7a52
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-07 09:02:11 -0300

    Completo fixture para insert-limit

--- test/fixtures/insert-limit.sql
+++ test/fixtures/insert-limit.sql
@@ -1,8 +1,29 @@
 create table "insert-limit" (
- "texto" character varying(5),
- "numero" integer
+ "texto" character varying(10),
+ "numero" integer,
+ primary key ("texto")
 );
 
 insert into "insert-limit" ("texto", "numero") values
  ('uno', 1),
+ ('dos, 2),
+ ('tres, 3),
+ ('cuatro, 4),
+ ('cinco, 5),
+ ('seis, 6),
+ ('siete, 7),
+ ('ocho, 8),
+ ('nueve, 9),
+ ('diez', 10);
+
+insert into "insert-limit" ("texto", "numero") values
+ ('once, 11),
+ ('doce, 12),
+ ('trece, 13),
+ ('catorce, 14),
+ ('quince, 15),
+ ('dieciseis, 16),
+ ('diecisiete, 17),
+ ('dieciocho, 18),
+ ('diecinueve, 19),
  ('veinte', 20);
