920694746251cc14a928788714e830cd92f91f6e
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-09 11:45:52 -0300

    Arreglo bug del insert segmentado. Close #26

--- test/fixtures/insert-limit.sql
+++ test/fixtures/insert-limit.sql
@@ -8,21 +8,27 @@ insert into "insert-limit" ("texto", "numero") values
  ('uno', 1),
  ('dos', 2),
  ('tres', 3),
- ('cuatro', 4),
+ ('cuatro', 4);
+
+insert into "insert-limit" ("texto", "numero") values
  ('cinco', 5),
  ('seis', 6),
  ('siete', 7),
- ('ocho', 8),
- ('nueve', 9),
- ('diez', 10);
+ ('ocho', 8);
 
 insert into "insert-limit" ("texto", "numero") values
+ ('nueve', 9),
+ ('diez', 10),
  ('once', 11),
- ('doce', 12),
+ ('doce', 12);
+
+insert into "insert-limit" ("texto", "numero") values
  ('trece', 13),
  ('catorce', 14),
  ('quince', 15),
- ('dieciseis', 16),
+ ('dieciseis', 16);
+
+insert into "insert-limit" ("texto", "numero") values
  ('diecisiete', 17),
  ('dieciocho', 18),
  ('diecinueve', 19),
