9160cbf831af90ebccf43e70328bd8bb9e63823b
Diego F.(EW7) <diegoefe@gmail.com>
2016-12-06 17:25:00 -0300

    Mas dates (#31)

--- test/fixtures/dates.sql
+++ test/fixtures/dates.sql
@@ -1,9 +1,10 @@
 create table "dates" (
  "date1" date,
  "date2" date,
+ "date3" date,
  primary key ("date1")
 );
 
-insert into "dates" ("date1", "date2") values
- ('2016/11/21', '2011-4-15'),
- ('1969/05/06', '1969-2-2');
+insert into "dates" ("date1", "date2", "date3") values
+ ('2016/11/21', '2011-4-15', '2/2/1969'),
+ ('1969/05/06', '1969-2-2', null);
