bff93c5016b528d7ec2f64a3e954116ef13ec903
Diego F.(EW7) <diegoefe@gmail.com>
2016-12-12 12:02:22 -0300

    binary_double para Oracle

--- test/fixtures/oracle-example-one.sql
+++ test/fixtures/oracle-example-one.sql
@@ -3,7 +3,7 @@ create table "oracle-example-one" (
  "int col" integer,
  "num col" number(8,6),
  "big col" long,
- "double col" number
+ "double col" binary_double
 );
 
 insert into "oracle-example-one" ("text col", "int col", "num col", "big col", "double col") values ('hello', 1, 3.141592, 1234567890, 1.12e-101);
--- test/fixtures/oracle-with-drop-table.sql
+++ test/fixtures/oracle-with-drop-table.sql
@@ -5,7 +5,7 @@ create table "oracle-with-drop-table" (
  "int col" integer,
  "num col" number(8,6),
  "big col" long,
- "double col" number
+ "double col" binary_double
 );
 
 insert into "oracle-with-drop-table" ("text col", "int col", "num col", "big col", "double col") values ('hello', 1, 3.141592, 1234567890, 1.12e-101);
