3191b80723dc4badbe3612f9ce5615e044539046
Diego F.(EW7) <diegoefe@gmail.com>
2016-12-26 16:15:09 -0300

    Agrego date a "all-types"

--- test/fixtures/all-types.sql
+++ test/fixtures/all-types.sql
@@ -4,10 +4,11 @@ create table "all-types" (
  "numero" numeric(8,6),
  "grande" bigint,
  "doble" double precision,
- "boleano" boolean
+ "boleano" boolean,
+ "fecha" date
 );
 
-insert into "all-types" ("texto", "entero", "numero", "grande", "doble", "boleano") values
- ('hello', 4, 3.141592, 1234567890, 1.12e-101, true),
- ('bye', 5, 3.141593, 1234567890, 1.12e-101, false),
- (null, null, null, 0, 0.0, null);
+insert into "all-types" ("texto", "entero", "numero", "grande", "doble", "boleano", "fecha") values
+ ('hello', 4, 3.141592, 1234567890, 1.12e-101, true, '2011-4-15'),
+ ('bye', 5, 3.141593, 1234567890, 1.12e-101, false, '14-2-1999'),
+ (null, null, null, 0, 0.0, null, null);
